4576a630f4ccd15d329221d6bd96754c7333069e
Corentin CS. SANNA <Corentin@ANTIBIA.priv>
2022-08-29 17:32:27 +0200

    animation name

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require("tailwindcss/plugin");
+const plugin = require('tailwindcss/plugin');
 
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
   plugins: [
-    plugin(function ({ addBase, theme }) {
+    plugin(function ({addBase, theme}) {
       addBase({
-        h1: { fontSize: theme("fontSize.2xl") },
-        h2: { fontSize: theme("fontSize.xl") },
-        h3: { fontSize: theme("fontSize.lg") },
+        h1: {fontSize: theme('fontSize.xs')},
+        h2: {fontSize: theme('fontSize.xl')},
+        h3: {fontSize: theme('fontSize.lg')},
       });
     }),
   ],
